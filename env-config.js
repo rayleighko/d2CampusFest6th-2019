ccf7c5c51f9cfba05fd5f50c55065004d7166f17
Rayleigh <gmj1197@gmail.com>
2019-01-20 20:25:16 +0900

    fix: Modify project information

--- env-config.js
+++ env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
   'process.env.BACKEND_URL': prod
-    ? 'https://contributhon.herokuapp.com'
+    ? 'https://d2campusfest6th2019.herokuapp.com'
     : `http://localhost:${process.env.PORT || 1337}`,
 };
